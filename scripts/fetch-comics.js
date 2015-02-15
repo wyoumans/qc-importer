5d114bff02181d039b380b296bc23243666c024d
William Youmans <will@williamyoumans.com>
2015-02-15 16:40:51 -0500

    fetching the feed

--- scripts/fetch-comics.js
+++ scripts/fetch-comics.js
@@ -1,26 +1,38 @@
 'use strict';
 
-var config  = require('../config')
-  , logger  = require('../lib').logger
+var config = require('../config')
+  , logger = require('../lib').logger
+  , _      = require('lodash')
   , async  = require('async')
-  , _       = require('lodash')
+  , parser = require('blindparser')
   ;
 
 (function() {
 
-  var comics = [];
+  parser.parseURL(config.atomURL, function(err, data) {
+    var comics = [];
 
-  logger.info('Beginning Comics Import');
-
-  async.each(comics, function(comic, done) {
-    console.log(comic);
-    done(null);
-  }, function(err) {
-    if (err) {
-      logger.error(err);
+    if (data && _.isArray(data.items)) {
+      comics = data.items;
     }
 
-    logger.info('Comic Import Complete');
-    process.exit();
+    logger.info('Beginning Comics Import (%d found)', comics.length);
+
+    async.each(comics, function(comic, done) {
+      var comicObj = {
+        title: _.isArray(comic.title) ? comic.title.join(' ') : comic.title
+      };
+
+      console.log(comicObj);
+
+      done(null);
+    }, function(err) {
+      if (err) {
+        logger.error(err);
+      }
+
+      logger.info('Comic Import Complete');
+      process.exit();
+    });
   });
 })();
